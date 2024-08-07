c72908a300b7a6b35b9690fd488070d3dd6b9988
unknown <piyushkhannavb@gmail.com>
2024-08-07 15:01:13 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,14 @@
+const express= require('express')
+const bodyParser= require('body-parser')
+const app= express()
+const port =  process.env.PORT|| 3000;
+app.use(bodyParser.urlencoded({extended: true}))
+
+app.get("/", function(req, res) {
+    console.log("New home request by:", req.hostname);
+    res.send("Ok :)")
+})
+
+app.listen(port, ()=> {
+    console.log("Server started at port:", port);
+})
