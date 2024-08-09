d45a72b98725d91d892d4e42f12f83c2a411f406
unknown <piyushkhannavb@gmail.com>
2024-08-09 23:23:53 +0530

    hui

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express= require("express")
 const bodyParser= require('body-parser')
 const app= express()
-const port =  process.env.PORT|| 3000;
+const port =  4040;
 app.use(bodyParser.urlencoded({extended: true}))
 
 app.get("/", function(req, res) {
